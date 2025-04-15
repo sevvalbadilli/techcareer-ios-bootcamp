b1c35f17e36fb1cbffe6bf492bf783b3da58feee
sevvalbadilli <sevvalbadilli0@gmail.com>
2025-04-15 11:44:33 +0300

    first commit

---
+++ iOSBootcamp-Task01/iOSBootcamp-Task01/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  iOSBootcamp-Task01
+//
+//  Created by ŞEVVAL on 14.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        label.font = UIFont(name: "Lobster-Regular", size: 32)
+        
+    }
+
+
+}
+
